ee301634dc6dc3ecf78b1e82c461efd6e9c2ac1c
Dave-nbg <74248265+Dave-nbg@users.noreply.github.com>
2024-06-15 11:51:38 +0200

    simple version of ToDoList

--- mijn-vue-app/src/views/AboutView.vue
+++ mijn-vue-app/src/views/AboutView.vue
@@ -1,5 +1,44 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <h1>Todo</h1>
+
+  <label :for="TodoItem">Fill in your todo</label>
+  <input :id="TodoItem" v-model="todoInput">
+  <input type="button" @click="addItem()" value="Add" />
+  <ul>
+    <li v-for="(x,index) in todoText" :key=x>{{ x.text }}
+      <input type="checkbox" value="{{x.completed}}">
+      <input type="button" @click="deleteItem(index)" value="X" />
+    </li>
+  </ul>
+
 </template>
+
+
+<script setup>
+import { ref } from 'vue';
+
+const todoText = ref([{text: 'test', completed: false},{text: 'tester', completed: false}]);
+const todoInput = ref('');
+
+function addItem() {
+  console.log(todoInput.value)
+  if(todoInput.value !== ""){
+    todoText.value.push({text: todoInput.value, completed: false});
+    todoInput.value = '';
+  }
+}
+
+function deleteItem(index){
+  todoText.value.splice(index,1)
+}
+
+
+
+
+
+</script>
+
+<script>
+
+</script>
+
